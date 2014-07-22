604b794df19768cd581673d5f137ec42843ebe44
seanboyd26 <sboye@allstate.com>
2014-07-22 20:23:33 +0100

    Rock Paper Scissors.py

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,20 @@
+#Rock Paper Scissors Game
+
+print("""
+          // // // // // // // // // // // // // // // // // // // // // 
+        //  Hello and welcome to Rock, Paper, Scissors. You will be    //
+        //  playing against the computer. Good Luck!                   //
+          // // // // // // // // // // // // // // // // // // // // // 
+""")             
+
+Player = 0
+Computer = 0
+
+print("Player Score=", Player)
+print("Computer Score=", Computer)
+lets_begin = input("Are you ready to begin?")
+print(lets_begin, "\nOkay, lets begin")
+
+
+
+input("\n\nPress the enter key to exit.")
