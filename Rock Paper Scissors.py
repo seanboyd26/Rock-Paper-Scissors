a7a53180bd64cbaf494a0ecd8fbb9d3a79dc4ca6
seanboyd26 <sboye@allstate.com>
2014-07-22 21:10:13 +0100

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -10,11 +10,16 @@ print("""
 Player = 0
 Computer = 0
 lets_begin = input("Are you ready to begin?")
-print(lets_begin, "\nOkay, lets begin")
+
+if lets_begin == "Yes":
+    print(lets_begin, "\nOkay, lets begin")
+elif lets_begin == "No":
+    print(lets_begin, "\nOhh, well let me know when you are ready!")
+
+print("Round 1")
 print("\nPlayer Score=", Player)
 print("\nComputer Score=", Computer)
 
 
 
 input("\n\nPress the enter key to exit.")
-
