785d6c6ef9184a14fa60cb727251a3c2453be4f1
seanboyd26 <sboye@allstate.com>
2014-07-23 16:36:48 +0100

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -11,15 +11,52 @@ Player = 0
 Computer = 0
 lets_begin = input("Are you ready to begin?")
 
+Round = 0
+
+    
 if lets_begin == "Yes":
-    print(lets_begin, "\nOkay, lets begin")
+    print(lets_begin, "\nOkay, lets begin, first to win 3 rounds is the ultimate winner")
 elif lets_begin == "No":
     print(lets_begin, "\nOhh, well let me know when you are ready!")
 
 print("Round 1")
 print("\nPlayer Score=", Player)
-print("\nComputer Score=", Computer)
+print("Computer Score=", Computer)
 
 
+player_choice = input("\nWould you like Rock, Paper or Scissors?.")
+
+if player_choice == "Rock" or player_choice == "Paper" or player_choice == "Scissors":
+    print(player_choice, "Ohhh, good pick!")
+else:
+    print("\nSorry that is not recognised try again")
+
+import random
+comp_choice = random.randint(1,3)
+
+if comp_choice == 1:
+    comp_choice = "Rock"
+elif comp_choice == 2:
+    comp_choice = "Paper"
+elif comp_choice == 3:
+    comp_choice = "Scissors"
+    print("\nOkay, I choose " + str(comp_choice))
+
+if player_choice == comp_choice:
+    print("\nOhhh, looks like its a tie")
+    Round = Round + 1
+elif (comp_choice == "Rock" and player_choice == "Paper") or (comp_choice == "Paper" and player_choice == "Scissors") or \
+(comp_choice == "Scissors" and player_choice == "Rock"):
+    Player == Player + 1
+    print("\nOhh, looks like you win this round" + "\nPlayer Score =", Player)
+    Round = Round + 1
+else:
+    Computer == Computer + 1
+    print("\nOppps, sorry looks like you lose this round!" + " Computer Score", Computer)
+    Round = Round + 1
+
+
+
+    
 
 input("\n\nPress the enter key to exit.")
